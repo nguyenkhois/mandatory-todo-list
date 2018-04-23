c7919b0c49b26a1d01047097b30ca54ec83a633a
Khoi Le <minarek@outlook.com>
2018-04-23 12:48:10 +0200

    Updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const WebpackNotifierPlugin = require("webpack-notifier");
 
 module.exports = {
@@ -26,6 +27,7 @@ module.exports = {
         extensions: ['*', '.js', '.jsx']
     },
     plugins: [
+        new webpack.DefinePlugin({'process.env.NODE_ENV':JSON.stringify('production')}),
         new WebpackNotifierPlugin({alwaysNotify: true})
     ],
     devServer: {
